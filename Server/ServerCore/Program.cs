86508b040df4f2cd5ff930fc8ab791103d51a3f8
MecaDragon <kvg753@gmail.com>
2022-09-30 17:33:40 +0900

    락이 끝나면 이벤트를 전달하는 방식 AutoResetEvent를 쓴 경우 커널모드로 작동하기 때문에 시스템콜 호출이 잦아 락 안에 하는 일이 별로 없으면 스핀락보다 오히려 느리다

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,43 +4,28 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class SpinLock // 존버 메타
+    class Lock // 존버 메타
     {
-        volatile int _locked = 0;
+        // bool <= 커널
+        AutoResetEvent _available = new AutoResetEvent(true);
         public void Acquire()
         {
-            while(true)
-            {
-                //int original = Interlocked.Exchange(ref _locked, 1);
-                //if (original == 0)
-                //    break;
-
-                //CAS Compare-And_Swap
-                int expected = 0;
-                int desired = 1;
-                if(Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
-                    break;
-
-                // 쉬다 올게
-                //Thread.Sleep(1); // 무조건 휴식 => 무조건 1ms 정도 쉬고 싶어요
-                //Thread.Sleep(0); // 조건부 양보 => 나보다 우선순위가 낮은 애들한테는 양보 불가 => 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인한테
-                Thread.Yield(); // 관대한 양보 => 관대하게 양보할테니, 지금 실행이 가능한 쓰레드가 있으면 실행하세요 => 실행 가능한 애가 없으면 남은 시간 소진
-            }
+            _available.WaitOne(); // 입장 시도
         }
 
         public void Release()
         {
-            _locked = 0;
+            _available.Set(); // flag = true
         }
     }
     class Program
     {
         static int _num = 0;
-        static SpinLock _lock = new SpinLock();
+        static Lock _lock = new Lock();
 
         static void Thread_1()
         {
-            for(int i = 0; i < 1000000; i++)
+            for(int i = 0; i < 10000; i++)
             {
                 _lock.Acquire();
                 _num++;
@@ -50,7 +35,7 @@ namespace ServerCore
 
         static void Thread_2()
         {
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 _lock.Acquire();
                 _num--;
