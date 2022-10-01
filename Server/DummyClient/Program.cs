5ab6f49c02054e1a6f377666b10c3e09658a18b0
MecaDragon <kvg753@gmail.com>
2022-10-01 12:53:59 +0900

    패킷 조립

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -9,14 +9,28 @@ namespace DummyClient
 {
     class GameSession : Session
     {
+        class Packet
+        {
+            public ushort size;
+            public ushort packetid;
+        }
+
         public override void OnConnected(EndPoint endPoint)
         {
             Console.WriteLine($"OnConnected : {endPoint}");
 
+            Packet packet = new Packet() { size = 4, packetid = 7 };
+
             // 보낸다
             for (int i = 0; i < 5; i++)
             {
-                byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i}");
+                ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+                byte[] buffer = BitConverter.GetBytes(packet.size);
+                byte[] buffer2 = BitConverter.GetBytes(packet.packetid);
+                Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+                Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+                ArraySegment<byte> sendBuff = SendBufferHelper.Close(packet.size);
+
                 Send(sendBuff);
             }
         }
