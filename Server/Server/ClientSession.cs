ab17d26a9496d412faee792ae44017c8d9dac295
MecaDragon <kvg753@gmail.com>
2022-10-01 13:53:01 +0900

    클라 서버 세션 분리, 패킷 직렬화

---
+++ Server/Server/ClientSession.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ServerCore;
+
+namespace Server
+{
+    class Packet
+    {
+        public ushort size;
+        public ushort packetid;
+    }
+
+    class PlayerInfoReq : Packet
+    {
+        public long playerid;
+    }
+
+    class PlayerInfoOK : Packet
+    {
+        public int hp;
+        public int attack;
+    }
+
+    public enum PacketID
+    {
+        PlayerInfoReq = 1,
+        PlayerInfoOK = 2,
+    }
+
+    class ClientSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            //Packet packet = new Packet() { size = 100, packetid = 10 };
+
+            //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            //byte[] buffer = BitConverter.GetBytes(packet.size);
+            //byte[] buffer2 = BitConverter.GetBytes(packet.packetid);
+            //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+            //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+            //ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
+
+            //Send(sendBuff);
+            Thread.Sleep(5000);
+            Disconnect();
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            ushort count = 0;
+
+            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+            count += 2;
+            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            count += 2;
+
+            switch((PacketID)id)
+            {
+                case PacketID.PlayerInfoReq:
+                    {
+                        long playerId = BitConverter.ToInt64(buffer.Array, buffer.Offset + count);
+                        count += 8;
+                        Console.WriteLine($"PlayerInfoReq: {playerId}");
+                    }
+                    break;
+            }
+
+            Console.WriteLine($"RecvPacketId: {id} , Size {size}");
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+}
